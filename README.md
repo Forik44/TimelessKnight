65034c2fa50112e7a033ae759b250accdf88e97d
Egor Prokofev <egor.prokofev.ru@gmail.com>
2022-10-03 19:30:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# STimelessKnight
+
+Developed with Unreal Engine 4
---
+++ Source/STimelessKnight/STimelessKnightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STimelessKnightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STIMELESSKNIGHT_API ASTimelessKnightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

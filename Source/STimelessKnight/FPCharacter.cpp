e1fd8cbac8c7dec1a21578c6b3b0566b35ada495
Egor Prokofev <egor.prokofev.ru@gmail.com>
2022-10-04 20:20:49 +0300

    Added FPPerson blank class

---
+++ Source/STimelessKnight/FPCharacter.cpp
@@ -0,0 +1,78 @@
+#include "FPCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "STimelessKnightGameModeBase.h"
+
+AFPCharacter::AFPCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	bUseControllerRotationYaw = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(GetRootComponent());
+	Camera->SetRelativeLocation(FVector(0, 0, 40));
+}
+
+void AFPCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPCharacter::Tick(float DeltaTime)
+{
+	
+}
+
+// Called to bind functionality to input
+void AFPCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("Hori", this, &AFPCharacter::HoriMove);
+	InputComponent->BindAxis("Vert", this, &AFPCharacter::VertMove);
+
+	InputComponent->BindAxis("HoriRot", this, &AFPCharacter::HoriRot);
+	InputComponent->BindAxis("VertRot", this, &AFPCharacter::VertRot);
+
+}
+
+void AFPCharacter::HoriMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorRightVector(), value);
+	}
+}
+
+void AFPCharacter::VertMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorForwardVector(), value);
+	}
+}
+
+void AFPCharacter::HoriRot(float value)
+{
+	if (value)
+	{
+		AddActorLocalRotation(FRotator(0, value, 0));
+	}
+}
+
+void AFPCharacter::VertRot(float value)
+{
+	if (value)
+	{
+		float Rotation = Camera->GetRelativeRotation().Pitch + value;
+
+		if (Rotation < 80 && Rotation > -80)
+		{
+			Camera->AddLocalRotation(FRotator(value, 0, 0));
+		}
+	}
+}
+
---
+++ Source/STimelessKnight/FPCharacter.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "FPCharacter.generated.h"
+
+UCLASS()
+class STIMELESSKNIGHT_API AFPCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	UCameraComponent* Camera;
+
+private:
+	void HoriMove(float value);
+	void VertMove(float value);
+
+	void HoriRot(float value);
+	void VertRot(float value);
+};
